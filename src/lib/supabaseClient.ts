7c8bfb552659828e45c73999cf7e504d39baddef
Gerrybp12 <gerry.bimaputra@gmail.com>
2025-06-09 13:36:59 +0700

    supabase

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://qwrenmtnnhfgxmrgfvnp.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+if (!supabaseKey) {
+  throw new Error("SUPABASE_KEY environment variable is not set");
+}
+const supabase = createClient(supabaseUrl, supabaseKey);
