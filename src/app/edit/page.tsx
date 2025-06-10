35ba95601e526a18c7f78929883bf386b01169cd
Gerrybp12 <gerry.bimaputra@gmail.com>
2025-06-10 18:50:20 +0700

    edit page

--- src/app/edit/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { createSupabaseService } from "@/services/supabaseService";
-import { useParams } from "next/navigation";
-
-export default function editPage() {
-  const params = useParams<{ id: string }>();
-  const supabaseService = createSupabaseService();
-}
